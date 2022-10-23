89d495fcb8a0d0fa6cc72a49e3fe199e78887d4e
yehuda-sanay <yehudasanay1@gmail.com>
2022-10-23 09:36:35 +0300

    /

--- src/Router.component.jsx
+++ src/Router.component.jsx
@@ -1,19 +1,12 @@
 import {Route, Routes} from 'react-router-dom'
 import { Home,Join, EthiopianWomenPage, EthiopianMenPage,SingIn,EthiopianGirlPage,EthiopianBoyPage,EthiopianCouplePage,EthiopianAccessoriesPage,EthiopianJewelryPage,JoinSingInPage,CartPage } from './component/pages'
 import EthiopianChoicePage from './component/pages/EthiopianChoicePage/EthiopianChocePage.component'
-import EthiopianMenProvaider from './contexts/EthiopianMenProvaider'
-import EthiopianWomenPrivaider from './contexts/EthiopianWomenPrivaider'
-import EthiopianGirlProvaider from './contexts/EthiopianGirlProvaider.component'
-import EthiopianBoyProvaider from './contexts/EthiopianBoyProvaider.component'
-import EthiopianCoupleProvaider from './contexts/EthiopianCoupleProvaider.component'
-import EthiopianAccessoriesProvaider from './contexts/EthiopianAccessoriesProvaider.component'
-import EthiopianJewelryProvaider from './contexts/EthiopianJewelryProvaider.component'
-// import {CartProvider} from 'react-use-cart'
-// import { ShoppingCartProvider } from './contexts/ShoppingCartContext'
+
+
 
 export default function Routing(){
     return(
-            // <CartProvider>
+            
             
         <Routes>
             <Route path='/' element={<Home/>}/>
@@ -32,6 +25,6 @@ export default function Routing(){
         </Routes>
         
         
-            // </CartProvider>
+            
     )
 }
